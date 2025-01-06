703330d851b2ad60d6c6d418c4dd8a10a1180fd1
vzyree619 <vzyree619@gmail.com>
2025-01-06 23:36:56 +0500

    Add Remove Duplicate leet code 26

---
+++ Array/RemoveDuplicates.java
@@ -0,0 +1,38 @@
+package Array;
+
+// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length. Do not allocate extra space for another array; you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example:
+
+// Input: nums = [1, 1, 2] 
+// Output: 2
+
+
+public class RemoveDuplicates {
+    public int removeD(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicates rd = new RemoveDuplicates();
+        int[] nums = {1, 1, 2, 2, 3, 3, 4, 4, 5};
+        int newLength = rd.removeD(nums);
+        System.out.println("New length: " + newLength);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < newLength; i++) {
+        System.out.print(nums[i] + " ");
+        }
+    }
+}
